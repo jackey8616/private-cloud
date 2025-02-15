f5fa31a96adc607b81547e1dac5d6abc56eef4cd
Clooooode <12930377+jackey8616@users.noreply.github.com>
2025-02-15 19:07:19 +0800

    feat(module): seeker service (#1)

--- main.tf
+++ main.tf
@@ -2,9 +2,15 @@ module "PyFun" {
   source                             = "./pyfun"
   GitHub-OIDC-Arn                    = aws_iam_openid_connect_provider.GitHub.arn
   Lambda-EdgeFunctionExecuteRole-Arn = aws_iam_policy.AWSLambdaEdgeExecutionRole.arn
-  ECR-Image-Sha                      = "sha256:3921acf6fb08f497d6eb521f047e4797b95783a143a74654bd63368dfc8a93f4"
-  Cert_Key_Path                      = "pyfun/certs/pyfun-backend-v2.clo5de.info.key"
-  Cert_Pem_Path                      = "pyfun/certs/pyfun-backend-v2.clo5de.info.pem"
+  ECR-Image-Sha                      = var.pyfun-aws-ecr-image-sha
+  Cert_Key_Path                      = var.pyfun-aws-cert-key-path
+  Cert_Pem_Path                      = var.pyfun-aws-cert-pem-path
+}
+
+module "Seeker" {
+  source = "./seeker"
+  gcp-project-id = var.seeker-gcp-project-id
+  gcp-billing-account = var.seeker-gcp-billing-account
 }
 
 resource "aws_servicecatalogappregistry_application" "terraform" {
